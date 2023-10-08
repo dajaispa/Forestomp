8232d22682e5ef32dd3b418d12ee17e2a01ee1bd
damini <daminijais610@gmail.com>
2023-10-09 04:33:16 +0530

    branch_change

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameSession : MonoBehaviour
+{
+    [SerializeField] int playerLives = 3;
+    [SerializeField] int score = 0;
+    [SerializeField] TextMeshProUGUI livesText;
+    [SerializeField] TextMeshProUGUI scoreText;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        int numGameSessions = FindObjectsOfType<GameSession>().Length;
+        if(numGameSessions > 1){
+            Destroy(gameObject);
+        }
+        else{
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start(){
+        livesText.text = playerLives.ToString();
+        scoreText.text = score.ToString();
+    }
+
+    public void AddtoScore(int pointsToAdd){
+        score += pointsToAdd;
+        scoreText.text = score.ToString();
+    }
+
+    public void ProcessPlayerDeath(){
+       if(playerLives > 1){
+         TakeLife();
+       }
+       else{
+        ResetGameSession();
+       }
+    }
+
+    void ResetGameSession(){
+        FindObjectOfType<ScenePersist>().ResetScenePersist();
+        SceneManager.LoadScene(0);
+        Destroy(gameObject);
+    }
+
+    void TakeLife(){
+        playerLives--;
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+        livesText.text = playerLives.ToString();
+    }
+
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 5f;
+    [SerializeField] Vector2 deathkick = new Vector2(10f, 10f);
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform gun;
+    Vector2 moveInput;
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
+    CapsuleCollider2D myBodyCollider;
+    BoxCollider2D myFeetCollider;
+    float gravityScaleAtStart;
+    bool isAlive = true;
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
+        myFeetCollider = GetComponent<BoxCollider2D>();
+        gravityScaleAtStart = myRigidBody.gravityScale;
+    }
+
+    void Update()
+    {
+     if(!isAlive){ return; }
+     Run(); 
+     FlipSprite();  
+     ClimbLadder();
+     Die();
+    }
+
+    void OnFire(InputValue value){
+         if(!isAlive){return;}
+         Instantiate(bullet, gun.position, transform.rotation); 
+    }
+
+    void OnMove(InputValue value){
+      if(!isAlive){ return; }
+      moveInput = value.Get<Vector2>();
+    }
+
+    void Run(){
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
+    }
+
+    void OnJump(InputValue value){
+       if(!isAlive){ return; }
+       if(!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))){
+        return;
+       }
+       if(value.isPressed){
+        myRigidBody.velocity += new Vector2(0f, jumpSpeed);
+       }
+    }
+
+    void ClimbLadder(){
+        if(!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"))){
+           myRigidBody.gravityScale = gravityScaleAtStart;
+           myAnimator.SetBool("isClimbing",false);
+           return;
+        }
+        Vector2 climbVelocity = new Vector2(myRigidBody.velocity.x, moveInput.y * climbSpeed);
+        myRigidBody.velocity = climbVelocity;
+        myRigidBody.gravityScale = 0f;
+        bool playerHasVerticalSpeed = Mathf.Abs(myRigidBody.velocity.y) > Mathf.Epsilon;
+        myAnimator.SetBool("isClimbing", playerHasVerticalSpeed);
+    }
+
+    void FlipSprite(){
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if(playerHasHorizontalSpeed){
+        transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x),1f);
+        }
+    }
+
+    void Die(){
+        if(myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemies", "Hazards"))){
+            isAlive = false;
+            myAnimator.SetTrigger("Dying");
+            myRigidBody.velocity = deathkick;
+            FindObjectOfType<GameSession>().ProcessPlayerDeath();
+        }
+    }
+}
